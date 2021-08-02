5cb489bf63fa27be6dd1ff84b8c00a78025223c3
nirch <nirh2@yahoo.com>
2021-08-02 13:37:50 +0300

    new recipe buttonn opens a modal

--- src/pages/RecipesPage/RecipesPage.jsx
+++ src/pages/RecipesPage/RecipesPage.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { useState } from 'react';
+import { Button, Col, Container, Modal, Row } from 'react-bootstrap';
 import { Redirect } from 'react-router-dom';
 import RecipeCard from '../../components/RecipeCard/RecipeCard';
 import "./RecipesPage.css"
 
-function RecipesPage({activeUser, recipes}) {
-
+function RecipesPage({ activeUser, recipes }) {
+    const [showRecipeModal, setShowRecipeModal] = useState(false)
 
     if (!activeUser) {
-        return <Redirect to="/"/>
+        return <Redirect to="/" />
     }
 
     return (
@@ -16,16 +17,34 @@ function RecipesPage({activeUser, recipes}) {
             <Container>
                 <div className="heading">
                     <h1>{activeUser.fname}'s Recipes</h1>
-                    <Button>New Recipe</Button>
+                    <Button onClick={() => setShowRecipeModal(true)}>New Recipe</Button>
                 </div>
                 <Row>
-                    {recipes.map(recipe => 
+                    {recipes.map(recipe =>
                         <Col key={recipe.id} md={3} sm={6}>
-                            <RecipeCard recipe={recipe}/>
+                            <RecipeCard recipe={recipe} />
                         </Col>
                     )}
                 </Row>
             </Container>
+
+
+            <Modal show={showRecipeModal} onHide={() => setShowRecipeModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={() => setShowRecipeModal(false)}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={() => setShowRecipeModal(false)}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+
         </div>
     );
 }
