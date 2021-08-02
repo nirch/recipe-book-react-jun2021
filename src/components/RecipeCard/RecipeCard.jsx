8991f3780cfc7071350247290f753a374cdfc0eb
nirch <nirh2@yahoo.com>
2021-08-02 13:20:34 +0300

    RecipeCard design

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
+import './RecipeCard.css'
 
-function RecipeCard({recipe}) {
+function RecipeCard({ recipe }) {
     return (
-        <div>
-            <p>{recipe.name}</p>
+        <div className="c-recipe">
+            <Card>
+                <Card.Img variant="top" src={recipe.img} />
+                <Card.Body>
+                    <Card.Title>{recipe.name}</Card.Title>
+                    <Card.Text>{recipe.desc}</Card.Text>
+                </Card.Body>
+            </Card>        
         </div>
     );
 }
