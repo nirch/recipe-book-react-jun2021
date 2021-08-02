eae4d1510012ee8409714b7ea134f60e132aca41
nirch <nirh2@yahoo.com>
2021-08-02 11:12:51 +0300

    login page design

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './LoginPage.css'
 
 function LoginPage(props) {
     return (
         <div className="p-login">
-            <Container>
-                LoginPage
-            </Container>
+            <h1>Login to Recipe Book</h1>
+            <p>or <Link to="/signup">create an account</Link></p>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <div className="d-grid gap-2">
+                    <Button variant="success" type="button">
+                        Login
+                    </Button>
+                </div>
+            </Form>
         </div>
     );
 }
