195e6bb58dcdd3d6f9190064759f1b9db9237cca
nirch <nirh2@yahoo.com>
2021-08-02 09:43:32 +0300

    project skeleton cleanup bootstrap adding 3 pages (home, login, recipes)

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function HomePage(props) {
+    return (
+        <div className="p-home">
+            <Container>
+                HomePage
+            </Container>
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LoginPage(props) {
+    return (
+        <div className="p-login">
+            LoginPage
+        </div>
+    );
+}
+
+export default LoginPage;
