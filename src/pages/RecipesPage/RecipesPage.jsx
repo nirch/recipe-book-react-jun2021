753386f1b968cf43ed82b230ca367237c291eeaa
nirch <nirh2@yahoo.com>
2021-08-11 09:39:32 +0300

    installing chartjs + rendering hard coded pie chart

--- src/pages/RecipesPage/RecipesPage.jsx
+++ src/pages/RecipesPage/RecipesPage.jsx
@@ -6,10 +6,40 @@ import NewRecipeModal from '../../components/NewRecipeModal/NewRecipeModal';
 import RecipeCard from '../../components/RecipeCard/RecipeCard';
 import ActiveUserContext from '../../shared/ActiveUserContext';
 import "./RecipesPage.css"
+import { Pie } from 'react-chartjs-2';
 
-function RecipesPage({recipes, onNewRecipe}) {
+function RecipesPage({ recipes, onNewRecipe }) {
     const [showRecipeModal, setShowRecipeModal] = useState(false)
     const activeUser = useContext(ActiveUserContext);
+    
+    const chartData = {
+        
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+            {
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
+
 
     if (!activeUser) {
         return <Redirect to="/" />
@@ -29,8 +59,9 @@ function RecipesPage({recipes, onNewRecipe}) {
                         </Col>
                     )}
                 </Row>
+                <Pie data={chartData} />
             </Container>
-            <NewRecipeModal show={showRecipeModal} onClose={() => setShowRecipeModal(false)} onCreate={onNewRecipe}/>
+            <NewRecipeModal show={showRecipeModal} onClose={() => setShowRecipeModal(false)} onCreate={onNewRecipe} />
         </div>
     );
 }
